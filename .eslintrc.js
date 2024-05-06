1ca5e75148f10a5d81715033c24c7131d30de5ef
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-05-06 21:51:40 +0900

    린트 설정

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
