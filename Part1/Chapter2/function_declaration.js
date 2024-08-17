79cb5c7988bac9e1e555550e2de8a5cbe9f047a1
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-08-17 21:09:54 +0900

    2.4 함수 선언식

---
+++ Part1/Chapter2/function_declaration.js
@@ -0,0 +1,10 @@
+function awesomeFunction(coolThings) {
+  // ...
+  return amazingStuff;
+}
+
+/**
+ * 함수 선언이란 이름은 이 함수가 다른 문의 표현식이 아니라 문 자체이기 때문에 붙게 되었습니다.
+ * 함수 선언으로 정의한 함수 awesomeFunction은 식별자 awesomeFunction과 실제 함수를 나타내는 값의 연관이
+ * 코드 실행 단계가 아닌 컴파일 단계에서 맺어집니다.
+ */
