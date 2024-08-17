f0602b9b2356d83918d6d90151c8a40f41bab5b2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-08-17 21:14:26 +0900

    2.4 함수 표현식

---
+++ Part1/Chapter2/function_expression.js
@@ -0,0 +1,10 @@
+const awesomeFunction = function(coolThings) {
+  // ...
+  return amazingStuff;
+}
+
+/**
+ * 함수 표현식은 함수와 함수 식별자가 코드가 실행되기 전까지는 관계를 맺지 않습니다.
+ * JS에서 함수는 할당 가능하고 어디든 전달 가능한 값이라는 특징은 매우 중요합니다.
+ * JS에서 함수는 객체의 한 종류입니다.
+ */
